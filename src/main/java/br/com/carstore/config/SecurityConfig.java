b566b0052251a3a69383a3d9043c344c7eceb171
Tiago-Mendoza <tiagomzdev@gmail.com>
2026-05-13 23:27:46 -0300

    atualizando as classes

--- src/main/java/br/com/carstore/config/SecurityConfig.java
+++ src/main/java/br/com/carstore/config/SecurityConfig.java
@@ -2,19 +2,52 @@ package br.com.carstore.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
 @EnableWebSecurity
 
 public class SecurityConfig {
+
+    private final JwtRequestFilter jwtRequestFilter;
+
+    public SecurityConfig(@Lazy JwtRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
+
+        // Regra 1: Configuração para API (Stateless) - Rotas /api/**
+        http.securityMatcher("/api/**") // Aplica ESTA regra SOMENTE a /api/**
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Sem sessão no servidor
+                )
+
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll() // Endpoint de Login API é público
+                        .anyRequest().authenticated() // Todas as outras rotas API exigem autenticação
+                );
+
+
+        return http.build();
+
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -74,6 +107,11 @@ public class SecurityConfig {
         // Gerenciador em memória (apenas para testes)
         return new org.springframework.security.provisioning.InMemoryUserDetailsManager(user);
 
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
 }
--- src/main/java/br/com/carstore/controller/LoginController.java
+++ src/main/java/br/com/carstore/controller/LoginController.java
@@ -1,14 +1,48 @@
 package br.com.carstore.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import br.com.carstore.dto.CarDTO;
+import br.com.carstore.dto.CarResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-public class LoginController {
-    @GetMapping("/login")
-    public String login() {
+import br.com.carstore.service.CarService;
 
-        return "/admin/login"; // templates/login.html
+import java.util.List;
 
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+    @Autowired
+    private final CarService carService;
+
+    public RestController(CarService service) {
+        this.carService = service;
     }
+
+    @GetMapping("/api/cars")
+    public ResponseEntity<CarResponseBody> home() {
+        List<CarDTO> allCars = carService.findAll();
+        CarResponseBody carResponseBody = new CarResponseBody(allCars);
+        return ResponseEntity.ok(carResponseBody);
+    }
+
+    @PostMapping("/api/cars")
+    public ResponseEntity<CarDTO> createCar(@RequestBody CarDTO car) {
+        this.carService.save(car);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/api/cars/{id}")
+    public ResponseEntity<CarDTO> deleteCar(@PathVariable String id) {
+        this.carService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/api/cars/{id}")
+    public ResponseEntity<CarDTO> updateCar(@PathVariable String id, @RequestBody CarDTO carDTO) {
+        this.carService.update(id, carDTO);
+        return ResponseEntity.ok(carDTO);
+    }
+
 }
